d42032a9a11b2b582e8d66de0b717c82559d8dc7
zhaowebb <zhaowebb@gmail.com>
2017-12-30 14:59:53 -0500

    first commit

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int DoubleMe(int number){
+    return number * 2;
+}
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << DoubleMe(33) << endl;
+    return 0;
+}
